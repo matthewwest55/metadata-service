903b1def2f7377d7a0927f6649a09a9dd9ef2e6c
Matthew West <matthewwest55@gmail.com>
2025-05-12 18:37:43 +0000

    Subscribe side API endpoint

--- src/mds/agg_mds/query.py
+++ src/mds/agg_mds/query.py
@@ -4,9 +4,167 @@ from mds import config
 from mds.agg_mds import datastore
 from typing import Any, Dict, List
 from pydantic import BaseModel
+import redis
+import threading
+import asyncio
+from urllib.parse import urlparse
+import json
+from mds.agg_mds.commons import MDSInstance, ColumnsToFields, Commons, parse_config
+from typing import Any, Optional
+from pathlib import Path
 
 mod = APIRouter()
+url_parts = urlparse(config.ES_ENDPOINT)
 
+def parse_config_from_file(path: Path) -> Optional[Commons]:
+    if not path.exists():
+        logger.error(f"configuration file: {path} does not exist")
+        return None
+    try:
+        return parse_config(path.read_text())
+    except IOError as ex:
+        logger.error(f"cannot read configuration file {path}: {ex}")
+        raise ex
+
+commons = parse_config_from_file(Path("./agg_mds_config.json"))
+
+async def populate_metadata(name: str, common, results, use_temp_index=False):
+    await datastore.init(hostname=url_parts.hostname, port=url_parts.port)
+    mds_arr = [{k: v} for k, v in results.items()]
+
+    total_items = len(mds_arr)
+
+    if total_items == 0:
+        logger.warning(f"populating {name} aborted as there are no items to add")
+        return
+
+    tags = {}
+
+    # inject common_name field into each entry
+    for x in mds_arr:
+        key = next(iter(x.keys()))
+        entry = next(iter(x.values()))
+
+        def normalize(entry: dict) -> Any:
+            # normalize study level metadata field names
+            if common.study_data_field != config.AGG_MDS_DEFAULT_STUDY_DATA_FIELD:
+                entry[config.AGG_MDS_DEFAULT_STUDY_DATA_FIELD] = entry.pop(
+                    common.study_data_field
+                )
+            # normalize variable level metadata field names, if available
+            if (
+                common.data_dict_field is not None
+                and common.data_dict_field != config.AGG_MDS_DEFAULT_DATA_DICT_FIELD
+            ):
+                entry[config.AGG_MDS_DEFAULT_DATA_DICT_FIELD] = entry.pop(
+                    common.data_dict_field
+                )
+
+            if (
+                not hasattr(common, "columns_to_fields")
+                or common.columns_to_fields is None
+            ):
+                return entry
+
+            for column, field in common.columns_to_fields.items():
+                if field == column:
+                    continue
+                if isinstance(field, ColumnsToFields):
+                    entry[config.AGG_MDS_DEFAULT_STUDY_DATA_FIELD][
+                        column
+                    ] = field.get_value(entry[config.AGG_MDS_DEFAULT_STUDY_DATA_FIELD])
+                else:
+                    if field in entry[config.AGG_MDS_DEFAULT_STUDY_DATA_FIELD]:
+                        entry[config.AGG_MDS_DEFAULT_STUDY_DATA_FIELD][column] = entry[
+                            config.AGG_MDS_DEFAULT_STUDY_DATA_FIELD
+                        ][field]
+            return entry
+
+        entry = normalize(entry)
+
+        # add the common field, selecting the name or an override (i.e. commons_name) and url to the entry
+
+        entry[config.AGG_MDS_DEFAULT_STUDY_DATA_FIELD]["commons_name"] = (
+            common.commons_name
+            if hasattr(common, "commons_name") and common.commons_name is not None
+            else name
+        )
+
+        # add to tags
+        for t in entry[config.AGG_MDS_DEFAULT_STUDY_DATA_FIELD].get("tags") or {}:
+            if "category" not in t:
+                continue
+            if t["category"] not in tags:
+                tags[t["category"]] = set()
+            if "name" in t:
+                tags[t["category"]].add(t["name"])
+
+    # process tags set to list
+    for k, v in tags.items():
+        tags[k] = list(tags[k])
+
+    keys = list(results.keys())
+    info = {"commons_url": common.commons_url}
+
+    await datastore.update_metadata(name, mds_arr, keys, tags, info, use_temp_index)
+
+
+@mod.get("/aggregate/join")
+async def join_mesh(ip_address:str, hostname:str, channel_name:str):
+    # Will this lead to memory leaks if I don't close threads properly?
+    new_thread = threading.Thread(target=asyncio.run, args=(subscribe_to_commons(ip_address, hostname, channel_name),))
+    new_thread.start()
+    return "mesh joined"
+
+async def subscribe_to_commons(ip_address:str, hostname:str, channel_name:str):
+    # Setup connection to Redis
+    # Gonna hard-code one ip address for now, will fix with config later
+    redis_client = redis.Redis(host=ip_address, port=6379, db=0)
+    channel = channel_name
+
+    # 2. Make redis spin
+    pubsub = redis_client.pubsub()
+    pubsub.subscribe(channel)
+    print(f"Subscribed to {channel}. Waiting for messages...")
+    for message in pubsub.listen():
+        if message['type'] == 'message':
+            message_data = message['data'].decode('utf-8')
+            # print(f"Received: {message_data}")
+            message_array = message_data.split(" ", 2)
+            rest_route = message_array[0]
+            guid = message_array[1]
+            data = message_array[2]
+            # 3. Make switch statement to update ES according to redis updates
+            # post
+            if rest_route == "POST":
+                # print(f"Getting https://{hostname}.dev.planx-pla.net/mds/metadata/{guid}?data=True")
+                print("Got data, doing thing now...")
+                print(data)
+
+                # get the data
+                results = {guid: {}}
+                # response = httpx.get(f"https://{hostname}.dev.planx-pla.net/mds/metadata/{guid}?data=True")
+                json_data = json.loads(data)
+                results[guid].update(json_data)
+
+                # print(len(json_data))
+
+                # Add to ES
+                # for name, common in commons.gen3_commons.items():
+                for name, common in commons.adapter_commons.items():
+                    # print(common)
+                    # print(results)
+                    await populate_metadata(name, common, results, False)
+
+            # put
+            elif rest_route == "PUT":
+                print("PUT not implemented")
+                pass
+
+            # delete
+            elif rest_route == "DELETE":
+                print("DELETE not implemented")
+                pass
 
 @mod.get("/aggregate/commons")
 async def get_commons():
