ca0a8b61af99fbdff2c55ef46598163d399cec9e
Matthew West <matthewwest55@gmail.com>
2025-05-22 22:46:04 -0500

    debug statement

--- src/mds/redis_pub_sub.py
+++ src/mds/redis_pub_sub.py
@@ -5,5 +5,6 @@ def get_redis_client(host, port, db):
     return redis.Redis(host=host, port=port, db=db)
 
 def redis_publish(client: redis.Redis, channel:str, message:str):
+    print(message)
     client.publish(channel, message)
     client.xadd(channel, message)
