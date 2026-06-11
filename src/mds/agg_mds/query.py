51c7eeade94241f47e2b3a93b5019f61b601741b
Matthew West <matthewwest55@gmail.com>
2026-06-11 13:39:11 -0500

    wow im dumb

--- src/mds/agg_mds/query.py
+++ src/mds/agg_mds/query.py
@@ -209,7 +209,8 @@ agg_mds_subscription_pool = dict[str, threading.Thread]()
 
 @mod.post("/aggregate/subscribe-endpoint")
 async def update_mesh_metadata(request: Request):
-    message = await request.body().decode("utf-8")
+    message_bytes = await request.body()
+    message = message_bytes.decode("utf-8")
     fixed_string = message.replace("\n", "\\n")
     my_data = ast.literal_eval(fixed_string)
 
